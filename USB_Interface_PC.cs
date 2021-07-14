fe72663f808a483e2d9f125b2e5e25c4f2acf1b8
Ikhlas Amgad <37087069+IkhlasAmgad@users.noreply.github.com>
2021-07-14 23:22:09 +0200

    Add files via upload

---
+++ USB_Interface_PC.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using usbGenericHidCommunications;
+namespace USBPorject
+{
+    class USB_Interface_PC : usbGenericHidCommunication
+
+    {
+        public USB_Interface_PC(int vid, int pid)
+            : base(vid, pid)
+        {
+
+        }
+
+        public bool sendData(Byte []  data)
+        {
+            bool succees = writeRawReportToDevice(data);
+            return succees;
+        }
+            
+
+    }
+}
